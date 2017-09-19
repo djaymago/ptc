90aa23c7b7b1fe03d441b4670d219a4073446534
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2017-09-19 18:51:23 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+</head>
+
+<body>
+<div class="connect">
+    <button class="connect-btn">Connect Now!</button>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
