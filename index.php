91e12d7873f894afb45ca8a33a036d6dcf54c76c
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2017-09-25 18:13:14 +0800

    Update Poten-Cee

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
     <link rel="stylesheet" href="https://ptc-campaign.herokuapp.com/js/loader/style.css" media="all"/>
     <link rel="stylesheet" href="https://ptc-campaign.herokuapp.com/js/sticky/sticky.min.css" media="all"/>
 
-<!--    <link rel="stylesheet" href="css/style.css" media="all"/>-->
-<!--    <link rel="stylesheet" href="js/loader/style.css" media="all"/>-->
-<!--    <link rel="stylesheet" href="js/sticky/sticky.min.css" media="all"/>-->
+    <!--    <link rel="stylesheet" href="css/style.css" media="all"/>-->
+    <!--    <link rel="stylesheet" href="js/loader/style.css" media="all"/>-->
+    <!--    <link rel="stylesheet" href="js/sticky/sticky.min.css" media="all"/>-->
 
     <!--[if lt IE 9]> <script src="https://ptc-campaign.herokuapp.com/js/css3-mediaqueries.js"></script> <![endif]-->
     <script src="https://ptc-campaign.herokuapp.com/js/html5.js"></script>
@@ -222,19 +222,19 @@
     </section>
 </section>
 <footer></footer>
-<!--<script src="https://ptc-campaign.herokuapp.com/js/lib/jquery.min.js"></script>-->
-<!--<script src="https://ptc-campaign.herokuapp.com/js/lib/jquery-ui.min.js"></script>-->
-<!--<script src="https://ptc-campaign.herokuapp.com/js/plugins.js"></script>-->
-<!--<script src="https://ptc-campaign.herokuapp.com/js/custom.js"></script>-->
-<!--<script src="https://ptc-campaign.herokuapp.com/js/app_script.js"></script>-->
-<!--<script src="https://ptc-campaign.herokuapp.com/js/loader/script.js"></script>-->
-<!--<script src="https://ptc-campaign.herokuapp.com/js/sticky/sticky.min.js"></script>-->
-<script src="js/lib/jquery.min.js"></script>
-<script src="js/lib/jquery-ui.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/app_script.js"></script>
-<script src="js/loader/script.js"></script>
-<script src="js/sticky/sticky.min.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/lib/jquery.min.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/lib/jquery-ui.min.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/plugins.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/custom.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/app_script.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/loader/script.js"></script>
+<script src="https://ptc-campaign.herokuapp.com/js/sticky/sticky.min.js"></script>
+<!--<script src="js/lib/jquery.min.js"></script>-->
+<!--<script src="js/lib/jquery-ui.min.js"></script>-->
+<!--<script src="js/plugins.js"></script>-->
+<!--<script src="js/custom.js"></script>-->
+<!--<script src="js/app_script.js"></script>-->
+<!--<script src="js/loader/script.js"></script>-->
+<!--<script src="js/sticky/sticky.min.js"></script>-->
 </body>
 </html>
